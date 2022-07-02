e48d35a99e0983e72a79dcdd572d0670587fb236
Rodel <duterterodelb@gmail.com>
2022-07-02 19:04:46 +0800

    test deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cool as me!
+          Cool as me updated!
         </p>
 
       </header>
